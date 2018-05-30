b772f56ad5bc305e836211eb8d8d1cc35c1c5cdc
yxliaoyx <yxliaoyx@gmail.com>
2018-05-31 02:57:18 +0800

    first-person

--- OpenGL-homework-project/OpenGL-homework-project/OpenGL-homework-project.cpp
+++ OpenGL-homework-project/OpenGL-homework-project/OpenGL-homework-project.cpp
@@ -1,172 +1,235 @@
-#include <iostream>
-#include <windows.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
 #include <GL/glut.h>
-#include <cstdlib>
-#include <cmath>
-
+#include <iostream>
+#include <time.h>
 using namespace std;
 
+#define G -981//硉
+#define CrBB 0.95//瞴籔瞴確玒计
+#define CrWB 0.9//鲤籔瞴確玒计
+#define t 0.001//–盎丁
+#define Max 21//程瞴计
+
 int window_width = 1280, window_height = 720;
-
-GLfloat myPositionX = 0;
-GLfloat myPositionY = 10;
-GLfloat myPositionZ = 0;
-
-GLfloat myRotation = 0;
-
-GLfloat myLookUpDown = 0;
-
-GLfloat mouseLastX;
-GLfloat mouseLastY;
-
-int centerX;
-int centerY;
-
+int center_x = window_width / 2;
+int center_y = window_height / 2;
+GLfloat player_x = 0;
+GLfloat player_y = 0;
+GLfloat player_z = 0;
+GLfloat rotation = 0;
+GLfloat look_up_down = 0;
+GLfloat mouse_last_x = window_width / 2;
+GLfloat mouse_last_y = window_height / 2;
 bool reset_Mouse = false;
 
-void reshape(int window_width, int window_height) {
-	glViewport(0, 0, window_width, window_height);
-	gluPerspective(45, (GLfloat)window_width / (GLfloat)window_height, 1, 1000);
+struct Ball {
+	GLdouble x, y, z, vx, vy, vz, r;
+};
+Ball ball[Max];
+
+int n = 0;
+
+void RandBall(int n) {
+	ball[n].x = rand() % 900 - 450;
+	ball[n].y = rand() % 450;
+	ball[n].z = rand() % 900 - 450;
+	ball[n].vx = rand() % 4000 - 2000;
+	ball[n].vy = rand() % 4000 - 2000;
+	ball[n].vz = rand() % 4000 - 2000;
 }
 
-void display() {
+void init(void) {
+	mouse_last_x = 640;
+	srand(time(NULL));
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+}
+
+void DrawBall(Ball ball) {
+	glPushMatrix();
+	glTranslated(ball.x, ball.y, ball.z);
+	if (ball.r < 40) {
+		glColor3f(0, 0, 1);
+	}
+	else {
+		glColor3f(1, 0, 0);
+	}
+	glutSolidSphere(ball.r, 20, 20);
+	glPopMatrix();
+}
+
+void Collision(int i) {
+	for (int j = 0; j < n; j++) {
+		if (i != j) {
+			double dx = ball[i].x - ball[j].x;
+			double dy = ball[i].y - ball[j].y;
+			double dz = ball[i].z - ball[j].z;
+			double d = sqrt(dx * dx + dy * dy + dz * dz);
+
+			if (d <= ball[i].r + ball[j].r) {
+				double v1n = (ball[i].vx * dx + ball[i].vy * dy + ball[i].vz * dz) / d;//猭絬よ硉
+				double v1ix = v1n * dx / d;//猭絬よ硉xだ秖
+				double v1iy = v1n * dy / d;//猭絬よ硉yだ秖
+				double v1iz = v1n * dz / d;//猭絬よ硉zだ秖
+
+				double v2n = (ball[j].vx * dx + ball[j].vy * dy + ball[j].vz * dz) / d;
+				double v2ix = v2n * dx / d;
+				double v2iy = v2n * dy / d;
+				double v2iz = v2n * dz / d;
+
+				double m1 = pow(ball[i].r, 3);
+				double m2 = pow(ball[j].r, 3);
+
+				double v1fx = (m1 * v1ix + m2 * v2ix + CrBB * m2 * (v2ix - v1ix)) / (m1 + m2);
+				double v1fy = (m1 * v1iy + m2 * v2iy + CrBB * m2 * (v2iy - v1iy)) / (m1 + m2);
+				double v1fz = (m1 * v1iz + m2 * v2iz + CrBB * m2 * (v2iz - v1iz)) / (m1 + m2);
+
+				double v2fx = (m1 * v1ix + m2 * v2ix + CrBB * m1 * (v1ix - v2ix)) / (m1 + m2);
+				double v2fy = (m1 * v1iy + m2 * v2iy + CrBB * m1 * (v1iy - v2iy)) / (m1 + m2);
+				double v2fz = (m1 * v1iz + m2 * v2iz + CrBB * m1 * (v1iz - v2iz)) / (m1 + m2);
+
+				ball[i].vx = v1fx + ball[i].vx - v1ix;
+				ball[i].vy = v1fy + ball[i].vy - v1iy;
+				ball[i].vz = v1fz + ball[i].vz - v1iz;
+
+				ball[j].vx = v2fx + ball[j].vx - v2ix;
+				ball[j].vy = v2fy + ball[j].vy - v2iy;
+				ball[j].vz = v2fz + ball[j].vz - v2iz;
+			}
+		}
+	}
+	bool floor = false;
+	if (ball[i].x - ball[i].r <= -500 || ball[i].x + ball[i].r >= 500) {
+		ball[i].vx = -ball[i].vx * CrWB;
+	}
+	if (ball[i].y - ball[i].r <= -500 || ball[i].y + ball[i].r >= 500) {
+		ball[i].vy = -ball[i].vy * CrWB;
+		if (ball[i].y - ball[i].r <= -500) {
+			floor = true;
+		}
+	}
+	if (ball[i].z - ball[i].r <= -500 || ball[i].z + ball[i].r >= 500) {
+		ball[i].vz = -ball[i].vz * CrWB;
+	}
+	if (!floor) {
+		ball[i].vy += G * t;
+	}
+}
+
+void Drag(int i) {
+	double ax = ball[i].vx;
+	double ay = ball[i].vy;
+	double az = ball[i].vz;
+	ball[i].vx -= ax * t;
+	ball[i].vy -= ay * t;
+	ball[i].vz -= az * t;
+}
+
+void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(0, 0, 0, 0);
+	glEnable(GL_COLOR_MATERIAL);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-
-	glRotatef(myLookUpDown, 1, 0, 0);
-	glRotatef(myRotation, 0, 1, 0);
-	glTranslatef(myPositionX, myPositionY, myPositionZ);
-
-	//glBindTexture(GL_TEXTURE_2D, texture);
-	//cube.draw();
-	/*glPushMatrix();
+	gluLookAt(player_x, player_y, player_z, player_x + cos(rotation), player_y + look_up_down, player_z + sin(rotation), 0, 1, 0);
+	glPushMatrix();
 	glTranslated(0, 0, 0);
 	glColor3f(0, 1, 0);
-	glutWireSphere(100, 10, 10);
-	glPopMatrix();*/
-	glBegin(GL_QUADS);
-	glVertex3f(-100, 0, 100);
-	glVertex3f(-100, 0, -100);
-	glVertex3f(100, 0, -100);
-	glVertex3f(100, 0, 100);
-	glEnd();
+	glutWireSphere(100, 30, 30);
+	glPopMatrix();
 
-	//Floor
-	//glBindTexture(GL_TEXTURE_2D, texture);
-	/*glColor3f(1, 1, 1);
-	glBegin(GL_QUADS);
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(Floor.vertexs[0].x, Floor.vertexs[0].y, Floor.vertexs[0].z);
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(Floor.vertexs[1].x, Floor.vertexs[1].y, Floor.vertexs[1].z);
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(Floor.vertexs[2].x, Floor.vertexs[2].y, Floor.vertexs[2].z);
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(Floor.vertexs[3].x, Floor.vertexs[3].y, Floor.vertexs[3].z);
-	glEnd();*/
-
-	glutSwapBuffers();
+	for (int i = 0; i < n; i++) {
+		DrawBall(ball[i]);
+		Collision(i);
+		Drag(i);
+		ball[i].x += ball[i].vx * t;
+		ball[i].y += ball[i].vy * t;
+		ball[i].z += ball[i].vz * t;
+	}
+	glFlush();
 }
 
-void keyBoardFunc(unsigned char key, int x, int y) {
-	GLfloat rad = myRotation / 57.29577951;
+void reshape(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(30, (GLfloat)w / h, 1, 5000);
+}
+
+void keyboard(unsigned char key, int x, int y) {
 	switch (key) {
 	case 'w':
-		myPositionX += sin(rad);
-		myPositionZ += cos(rad);
+		player_x += cos(rotation);
+		player_z += sin(rotation);
 		break;
 
 	case 'a':
-		myPositionX += cos(rad);
-		myPositionZ -= sin(rad);
+		player_x += sin(rotation);
+		player_z -= cos(rotation);
 		break;
 
 	case 's':
-		myPositionX -= sin(rad);
-		myPositionZ -= cos(rad);
+		player_x -= cos(rotation);
+		player_z -= sin(rotation);
 		break;
 
 	case 'd':
-		myPositionX -= cos(rad);
-		myPositionZ += sin(rad);
+		player_x -= sin(rotation);
+		player_z += cos(rotation);
 		break;
 	}
 	glutPostRedisplay();
 }
 
+void Timer(int c) {
+	glutPostRedisplay();
+	glutTimerFunc(1, Timer, 0);
+}
+
 void mouseMovementFunc(int x, int y) {
 	if (reset_Mouse == false) {
-		int mouseDiffX = x - mouseLastX;
-		int mouseDiffY = y - mouseLastY;
+		int mouseDiffX = x - mouse_last_x;
+		int mouseDiffY = y - mouse_last_y;
+		
+		mouse_last_x = x;
+		mouse_last_y = y;
 
-		mouseLastX = x;
-		mouseLastY = y;
+		rotation += mouseDiffX * 0.01;
 
-		myRotation += mouseDiffX;
-		if (myRotation > 360) {
-			myRotation -= 360;
+		look_up_down -= mouseDiffY * 0.01;
+		if (look_up_down > 1) {
+			look_up_down = 1;
 		}
-		else if (myRotation < 0) {
-			myRotation += 360;
+		else if (look_up_down < -1) {
+			look_up_down = -1;
 		}
-		myLookUpDown += mouseDiffY;
-		if (myLookUpDown > 90) {
-			myLookUpDown = 90;
-		}
-		else if (myLookUpDown < -90) {
-			myLookUpDown = -90;
-		}
-
-		if ((int)mouseLastX != centerX && (int)mouseLastY != centerY) {
+		
+		if (mouse_last_x != center_x || mouse_last_y != center_y) {
 			reset_Mouse = true;
 		}
 	}
 	else if (reset_Mouse == true) {
-		glutWarpPointer(centerX, centerY);
-		mouseLastX = centerX;
-		mouseLastY = centerY;
+		glutWarpPointer(center_x, center_y);
+		mouse_last_x = center_x;
+		mouse_last_y = center_y;
 
 		reset_Mouse = false;
 	}
 	glutPostRedisplay();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(window_width, window_height);
-	glutCreateWindow("Project #5");
-
-	glMatrixMode(GL_PROJECTION);
-
-	glEnable(GL_DEPTH_TEST);
-
-	/*Floor.vertexs[0].x = 0.0f;
-	Floor.vertexs[0].y = -2.0f;
-	Floor.vertexs[0].z = 10.0f;
-	Floor.vertexs[1].x = -10.0f;
-	Floor.vertexs[1].y = -2.0f;
-	Floor.vertexs[1].z = 0.0f;
-	Floor.vertexs[2].x = 0.0f;
-	Floor.vertexs[2].y = -2.0f;
-	Floor.vertexs[2].z = -10.0f;
-	Floor.vertexs[3].x = 10.0f;
-	Floor.vertexs[3].y = -2.0f;
-	Floor.vertexs[3].z = 0.0f;*/
-
-	mouseLastX = centerX = window_width / 2;
-	mouseLastY = centerY = window_height / 2;
-
+	glutCreateWindow("Project #2");
 	glutSetCursor(GLUT_CURSOR_CROSSHAIR);
-
-	glutWarpPointer(mouseLastX, mouseLastY);
-
-	glutReshapeFunc(reshape);
+	glutWarpPointer(center_x, center_y);
+	init();
 	glutDisplayFunc(display);
-	glutKeyboardFunc(keyBoardFunc);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
 	glutPassiveMotionFunc(mouseMovementFunc);
-
+	glutTimerFunc(1, Timer, 0);
 	glutMainLoop();
-
 	return 0;
 }
