3d2c806df39b35c5c9b283df4b6d24d93be67d96
yxliaoyx <yxliaoyx@gmail.com>
2018-06-08 17:17:32 +0800

    no message

--- OpenGL-homework-project/OpenGL-homework-project/OpenGL-homework-project.cpp
+++ OpenGL-homework-project/OpenGL-homework-project/OpenGL-homework-project.cpp
@@ -22,17 +22,8 @@ int center_x = window_width / 2, center_y = window_height / 2;
 double player_x = room_length / 3, player_y = 0, player_z = room_length / 3, player_y_velocity = 0;
 double rotation = 4, look_up_down = 0;
 double mouse_last_x = window_width / 2, mouse_last_y = window_height / 2;
-double t = 0.01, reloading = 0;
+double t = 0.005, reloading = 0;
 bool reset_Mouse = false;
-
-void createBullet(Bullet & bullet) {
-	bullet.x = player_x + cos(rotation) * 10;
-	bullet.y = player_y + look_up_down * 10;
-	bullet.z = player_z + sin(rotation) * 10;
-	bullet.x_velocity = cos(rotation) * 100;
-	bullet.y_velocity = look_up_down * 100;
-	bullet.z_velocity = sin(rotation) * 100;
-}
 
 void createTeapothead(void) {
 	for (int i = 0; i < number_of_teapotheads; i++) {
@@ -55,9 +46,9 @@ void playerShoot(Bullet & bullet) {
 	bullet.x = player_x + cos(rotation) * 10;
 	bullet.y = player_y + look_up_down * 10;
 	bullet.z = player_z + sin(rotation) * 10;
-	bullet.x_velocity = cos(rotation) * 1500;
-	bullet.y_velocity = look_up_down * 1500;
-	bullet.z_velocity = sin(rotation) * 1500;
+	bullet.x_velocity = cos(rotation) * 1000;
+	bullet.y_velocity = look_up_down * 1000;
+	bullet.z_velocity = sin(rotation) * 1000;
 }
 
 void teapotheadShoot(Teapothead & teapothead, Bullet & bullet) {
@@ -65,18 +56,11 @@ void teapotheadShoot(Teapothead & teapothead, Bullet & bullet) {
 	bullet.y = teapothead.y;
 	bullet.z = teapothead.z;
 	bullet.x_velocity = player_x - teapothead.x;
-	bullet.z_velocity = player_z -teapothead.z;
+	bullet.y_velocity = player_y - teapothead.y;
+	bullet.z_velocity = player_z - teapothead.z;
 }
 
-//void DrawBullet(Bullet bullet) {
-//	glPushMatrix();
-//	glTranslated(bullet.x, bullet.y, bullet.z);
-//	glColor3f(0, 0, 1);
-//	glutSolidSphere(ball.r, 10, 10);
-//	glPopMatrix();
-//}
-
-bool edgeCollision(Bullet & bullet) {
+bool edgeCollision(Bullet bullet) {
 	int boundary = room_length / 2;
 	if (bullet.x < -boundary || bullet.x > boundary
 		|| bullet.y < -boundary || bullet.y > boundary
@@ -88,47 +72,18 @@ bool edgeCollision(Bullet & bullet) {
 	}
 }
 
-//void Collision(int i) {
-//	for (int j = 0; j < n; j++) {
-//		if (i != j) {
-//			double dx = ball[i].x - ball[j].x;
-//			double dy = ball[i].y - ball[j].y;
-//			double dz = ball[i].z - ball[j].z;
-//			double d = sqrt(dx * dx + dy * dy + dz * dz);
-//
-//			if (d <= ball[i].r + ball[j].r) {
-//				double v1n = (ball[i].vx * dx + ball[i].vy * dy + ball[i].vz * dz) / d;//法線方向的速度
-//				double v1ix = v1n * dx / d;//法線方向的速度的x分量
-//				double v1iy = v1n * dy / d;//法線方向的速度的y分量
-//				double v1iz = v1n * dz / d;//法線方向的速度的z分量
-//
-//				double v2n = (ball[j].vx * dx + ball[j].vy * dy + ball[j].vz * dz) / d;
-//				double v2ix = v2n * dx / d;
-//				double v2iy = v2n * dy / d;
-//				double v2iz = v2n * dz / d;
-//
-//				double m1 = pow(ball[i].r, 3);
-//				double m2 = pow(ball[j].r, 3);
-//
-//				double v1fx = (m1 * v1ix + m2 * v2ix + CrBB * m2 * (v2ix - v1ix)) / (m1 + m2);
-//				double v1fy = (m1 * v1iy + m2 * v2iy + CrBB * m2 * (v2iy - v1iy)) / (m1 + m2);
-//				double v1fz = (m1 * v1iz + m2 * v2iz + CrBB * m2 * (v2iz - v1iz)) / (m1 + m2);
-//
-//				double v2fx = (m1 * v1ix + m2 * v2ix + CrBB * m1 * (v1ix - v2ix)) / (m1 + m2);
-//				double v2fy = (m1 * v1iy + m2 * v2iy + CrBB * m1 * (v1iy - v2iy)) / (m1 + m2);
-//				double v2fz = (m1 * v1iz + m2 * v2iz + CrBB * m1 * (v1iz - v2iz)) / (m1 + m2);
-//
-//				ball[i].vx = v1fx + ball[i].vx - v1ix;
-//				ball[i].vy = v1fy + ball[i].vy - v1iy;
-//				ball[i].vz = v1fz + ball[i].vz - v1iz;
-//
-//				ball[j].vx = v2fx + ball[j].vx - v2ix;
-//				ball[j].vy = v2fy + ball[j].vy - v2iy;
-//				ball[j].vz = v2fz + ball[j].vz - v2iz;
-//			}
-//		}
-//	}
-//}
+bool bulletCollision(Bullet bullet_1, Bullet bullet_2) {
+	double x_distance = (bullet_1.x - bullet_2.x) * (bullet_1.x - bullet_2.x);
+	double y_distance = (bullet_1.y - bullet_2.y) * (bullet_1.y - bullet_2.y);
+	double z_distance = (bullet_1.z - bullet_2.z) * (bullet_1.z - bullet_2.z);
+	printf("%lf\n", x_distance + y_distance + z_distance);
+	if (x_distance + y_distance + z_distance <= 3) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -137,12 +92,6 @@ void display(void) {
 	glLoadIdentity();
 	gluLookAt(player_x, player_y, player_z, player_x + cos(rotation), player_y + look_up_down, player_z + sin(rotation), 0, 1, 0);
 	//gluLookAt(0, 6000, 0, 0, 0, 0, 1, 0, 0);
-
-	/*glPushMatrix();
-	glColor3f(0, 1, 0);
-	glTranslated(player_x, player_y, player_z);
-	glutWireSphere(100, 30, 30);
-	glPopMatrix();*/
 
 	glPushMatrix();
 	glColor3f(0.5, 0.5, 0);
@@ -163,12 +112,16 @@ void display(void) {
 	glTranslated(0, 0, room_length);
 	glutSolidCube(room_length);
 	glTranslated(-0 * 2, 0, -room_length * 2);
-	glutSolidCube(1000);
+	glutSolidCube(room_length);
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3f(0, 1, 0);
+	glTranslated(player_x + cos(rotation) * 10, player_y + look_up_down * 10, player_z + sin(rotation) * 10);
+	glutSolidCube(2);
 	glPopMatrix();
 
 	for (int i = 0; i < number_of_teapotheads; i++) {
-		//double line_cos = cos(teapothead[i].degree / 57.29577951);
-		//double line_sin = sin(teapothead[i].degree / 57.29577951);
 		glPushMatrix();
 		glTranslated(teapothead[i].x, teapothead[i].y, teapothead[i].z);
 		glColor3f(teapothead[i].red, teapothead[i].green, teapothead[i].blue);
@@ -176,15 +129,12 @@ void display(void) {
 			glRotatef(270, 0, 1, 0);
 		}
 		glutSolidTeapot(10);
-		glutWireCube(20);
 		glTranslated(0, -50, 0);
 		glRotatef(270, 1, 0, 0);
 		glutSolidCone(10, 50, 3, 1);
 		glPopMatrix();
 
-		printf("i %d ", i);
-		if (number_of_teapothead_bullets < max_bullet){
-			printf("FFF %d\n", number_of_teapothead_bullets);
+		if (number_of_teapothead_bullets < max_bullet && rand() % number_of_teapotheads == i) {
 			teapotheadShoot(teapothead[i], teapothead_bullet[number_of_teapothead_bullets]);
 			number_of_teapothead_bullets++;
 		}
@@ -212,6 +162,16 @@ void display(void) {
 		player_bullet[i].y += player_bullet[i].y_velocity * t;
 		player_bullet[i].z += player_bullet[i].z_velocity * t;
 
+		for (int j = 0; j < number_of_teapothead_bullets; j++) {
+			if (bulletCollision(player_bullet[i], teapothead_bullet[j])) {
+				number_of_player_bullets--;
+				player_bullet[i] = player_bullet[number_of_player_bullets];
+				number_of_teapothead_bullets--;
+				teapothead_bullet[i] = teapothead_bullet[number_of_teapothead_bullets];
+				break;
+			}
+		}
+		
 		if (edgeCollision(player_bullet[i])) {
 			number_of_player_bullets--;
 			player_bullet[i] = player_bullet[number_of_player_bullets];
@@ -229,23 +189,25 @@ void display(void) {
 		teapothead_bullet[i].y += teapothead_bullet[i].y_velocity * t;
 		teapothead_bullet[i].z += teapothead_bullet[i].z_velocity * t;
 
+		for (int j = 0; j < number_of_player_bullets; j++) {
+			if (bulletCollision(player_bullet[j], teapothead_bullet[i])) {
+				number_of_player_bullets--;
+				player_bullet[i] = player_bullet[number_of_player_bullets];
+				number_of_teapothead_bullets--;
+				teapothead_bullet[i] = teapothead_bullet[number_of_teapothead_bullets];
+				break;
+			}
+		}
+
 		if (edgeCollision(teapothead_bullet[i])) {
-			printf("RRR %d\n", i);
-			printf("number_of_player_bullets %d\n", number_of_teapothead_bullets);
 			number_of_teapothead_bullets--;
 			teapothead_bullet[i] = teapothead_bullet[number_of_teapothead_bullets];
-			printf("number_of_player_bullets %d\n", number_of_teapothead_bullets);
-			printf("%lf %lf\n", teapothead_bullet[0].x, teapothead_bullet[0].z);
-			printf("%lf %lf\n", teapothead_bullet[1].x, teapothead_bullet[1].z);
-			printf("%lf %lf\n", teapothead_bullet[2].x, teapothead_bullet[2].z);
-			printf("%lf %lf\n", teapothead_bullet[3].x, teapothead_bullet[3].z);
-			printf("%lf %lf\n", teapothead_bullet[4].x, teapothead_bullet[4].z);
 		}
 	}
 
 	player_y += player_y_velocity * t;
 	if (player_y > 0) {
-		player_y_velocity -= 98 * t;
+		player_y_velocity -= 980 * t;
 	}
 	else {
 		player_y = 0;
@@ -259,7 +221,6 @@ void display(void) {
 		reloading = 0;
 		glutSetCursor(GLUT_CURSOR_CROSSHAIR);
 	}
-	printf("%d", number_of_teapothead_bullets);
 	glFlush();
 }
 
@@ -292,8 +253,8 @@ void specialKey(int key, int x, int y) {
 }
 
 void keyboard(unsigned char key, int x, int y) {
-	double cos_rotation = cos(rotation) * 5;
-	double sin_rotation = sin(rotation) * 5;
+	double cos_rotation = cos(rotation) * 2;
+	double sin_rotation = sin(rotation) * 2;
 	switch (key) {
 	case 'w':
 		player_x += cos_rotation;
@@ -313,16 +274,11 @@ void keyboard(unsigned char key, int x, int y) {
 		break;
 	case ' ':
 		if (player_y == 0) {
-			player_y_velocity = 50;
+			player_y_velocity = 500;
 		}
 		break;
 	}
 	glutPostRedisplay();
-}
-
-void Timer(int c) {
-	glutPostRedisplay();
-	glutTimerFunc(100, Timer, 0);
 }
 
 void mouseButton(int button, int state, int x, int y) {
@@ -341,8 +297,8 @@ void mouseMove(int x, int y) {
 		double distance_y = y - mouse_last_y;
 		mouse_last_x = x;
 		mouse_last_y = y;
-		rotation += distance_x * 0.005;
-		look_up_down -= distance_y * 0.005;
+		rotation += distance_x * 0.001;
+		look_up_down -= distance_y * 0.001;
 
 		if (look_up_down > 1) {
 			look_up_down = 1;
@@ -381,7 +337,6 @@ int main(int argc, char** argv) {
 	glutKeyboardFunc(keyboard);
 	glutMouseFunc(mouseButton);
 	glutPassiveMotionFunc(mouseMove);
-	//glutTimerFunc(10, Timer, 0);
 
 	glutMainLoop();
 	return 0;
